4437e11d97b59f8baaa991d4f992ab97d57e8f67
HamzaOran <60844909+HamzaOran@users.noreply.github.com>
2024-01-21 10:32:54 +0300

    ff

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const randomWord = words[randomIndex];
 
   return (
-    <div>
+    <div className='bg-white'>
       <Wordle wordle={randomWord} />
     </div>
   );
